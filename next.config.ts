80baca7585b7b5d472e1224601c395769587ce64
rakotoarison landry <rakotorisonlandry@gmail.com>
2026-03-05 11:24:18 +0100

    fix: simplify next.config.ts by removing experimental options and cleaning up output configuration

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export"
 };
-
-module.exports ={
-  experimental:{
-    serverActions:true,
-  }
-}
 export default nextConfig;
